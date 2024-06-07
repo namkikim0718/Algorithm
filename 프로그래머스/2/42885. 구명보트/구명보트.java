7bb8bf46d3b9df31c9e13c17d22a89198698244b
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2024-06-07 18:06:20 +0900

    [level 2] Title: 구명보트, Time: 8.80 ms, Memory: 53.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        
+        Arrays.sort(people);
+        int lt = 0;
+        int rt = people.length - 1;
+        
+        while (lt <= rt) {
+            if (people[lt] + people[rt] > limit) {
+                answer++;
+                rt--;
+            } else {
+                answer++;
+                rt--;
+                lt++;
+            }
+        }
+        
+        return answer;
+    }
+}
