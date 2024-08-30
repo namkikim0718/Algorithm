cbe50b7e32841c37622a55251362ffb6076e427a
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2024-08-30 16:33:21 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.12 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,34 +1,72 @@
 import java.util.*;
 
 class Solution {
+    
+    public int strToNum(StringBuilder sb) {
+        int result = -1;
+        switch (sb.toString()) {
+            case "zero" : 
+                result = 0;
+                break;
+            case "one" : 
+                result = 1;
+                break;
+            case "two" : 
+                result = 2;
+                break;
+            case "three" :
+                result = 3;
+                break;
+            case "four" : 
+                result = 4;
+                break;
+            case "five" : 
+                result = 5;
+                break;
+            case "six" : 
+                result = 6;
+                break;
+            case "seven" : 
+                result = 7;
+                break;
+            case "eight" :
+                result = 8;
+                break;
+            case "nine" : 
+                result = 9;
+                break;
+        }
+        return result;
+    }
+    
     public int solution(String s) {
-        int answer = 0;
-        String answerStr = "";
+        StringBuilder answer = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
         
-        String[] str = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        
-        int idx = 0;
-        String tmp = "";
-        while (idx < s.length()) {
-            char now = s.charAt(idx);
-            if (Character.isDigit(now)) {
-                answerStr += now;
-            } else {
-                tmp += now;
-                for (int i = 0; i < str.length; i++) {
-                    String x = str[i];
-                    if (x.equals(tmp)) {
-                        answerStr+= i;
-                        tmp = "";
-                        break;
-                    }
-                }
+        for (char x : s.toCharArray()) {
+            // 숫자면 answer에 바로 추가
+            if (Character.isDigit(x)) {
+                answer.append(x);
+                continue;
             }
-            idx++;
+            
+            // sb에 글자를 모아둠
+            sb.append(x);
+            // 숫자로 변환할 수 있는지 체크
+            int nowN = strToNum(sb);
+            
+            // 숫자로 변환못하면 넘어감
+            if (nowN == -1) {
+                continue;
+            }
+            
+            // 숫자로 변환되면 answer에 추가하고 sb 초기화
+            answer.append(nowN);
+            sb = new StringBuilder();
+            
         }
         
-        answer = Integer.parseInt(answerStr);
         
-        return answer;
+        return Integer.parseInt(answer.toString());
     }
 }
