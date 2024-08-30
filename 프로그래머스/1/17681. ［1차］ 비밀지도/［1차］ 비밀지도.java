fbd8db9f487f62792c5befb975c02eed7ee80a0f
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2024-08-30 17:28:12 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.26 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,49 +1,46 @@
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
+        
+        int[][] graph1 = new int[n][n];
+        int[][] graph2 = new int[n][n];
+        
         String[] answer = new String[n];
         
-        int[][] map = new int[n][n];
-        
-        
+        // graph1에 arr1을 0과 1로 된 이차원 배열로 저장
         for (int i = 0; i < n; i++) {
-            int x = arr1[i];
-            int y = arr2[i];
-            String lineX = Integer.toBinaryString(x);
-            String lineY = Integer.toBinaryString(y);
-            
-            if (lineX.length() < n) {
-                int sizeX = n - lineX.length();
-                for (int k = 0; k < sizeX; k++) {
-                    lineX = "0" + lineX;
-                }
+            String binaryStr = Integer.toBinaryString(arr1[i]);
+            for (int k = 0; k < n - binaryStr.length(); k++) {
+                graph1[i][k] = 0;
             }
-            if (lineY.length() < n) {
-                int sizeY = n - lineY.length();
-                for (int k = 0; k < sizeY; k++) {
-                    lineY = "0" + lineY;
-                }
-            }
-            
-            for (int j = 0; j < n; j++) {
-                if (lineX.charAt(j) == '0' && lineY.charAt(j) == '0') {
-                    map[i][j] = 0;   
-                }
-                else {
-                    map[i][j] = 1;
-                }
+            int idx = n - binaryStr.length();
+            for (char x : binaryStr.toCharArray()) {
+                graph1[i][idx++] = Character.getNumericValue(x);
             }
         }
         
+        // graph2에 arr2를 0과 1로 된 이차원 배열로 저장
         for (int i = 0; i < n; i++) {
-            String tmp = "";
-            for (int j = 0; j < n; j++) {
-                if (map[i][j] == 0) {
-                    tmp += " ";
-                } else {
-                    tmp += "#";
-                }
+            String binaryStr = Integer.toBinaryString(arr2[i]);
+            for (int k = 0; k < n - binaryStr.length(); k++) {
+                graph2[i][k] = 0;
             }
-            answer[i] = tmp;
+            int idx = n - binaryStr.length();
+            for (char x : binaryStr.toCharArray()) {
+                graph2[i][idx++] = Character.getNumericValue(x);
+            }
+        }
+        
+        // 두개의 이차원 배열을 순회하며 공백과 벽으로 이루어진 answer 만들기
+        for (int i = 0; i < n; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < n; j++) {
+                if (graph1[i][j] == 0 && graph2[i][j] == 0) {
+                    sb.append(" ");
+                    continue;
+                }
+                sb.append("#");
+            }
+            answer[i] = sb.toString();
         }
         
         return answer;
