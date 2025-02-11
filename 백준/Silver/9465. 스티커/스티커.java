5d0521c74b81d727117ad8465b36fcab5174e7bb
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2025-02-11 16:10:05 +0900

    [Silver I] Title: 스티커, Time: 736 ms, Memory: 120496 KB -BaekjoonHub

---
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            int n = Integer.parseInt(br.readLine());
+            int[][] stickers = new int[2][n+1];
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 1; i <= n; i++) {
+                stickers[0][i] = Integer.parseInt(st.nextToken());
+            }
+
+            st = new StringTokenizer(br.readLine());
+            for (int i = 1; i <= n; i++) {
+                stickers[1][i] = Integer.parseInt(st.nextToken());
+            }
+
+            int[][] dp = new int[2][n+1];
+            dp[0][1] = stickers[0][1];
+            dp[1][1] = stickers[1][1];
+
+            for (int i = 2; i <= n; i++) {
+                dp[0][i] = Math.max(dp[1][i-1], dp[1][i-2]) + stickers[0][i];
+                dp[1][i] = Math.max(dp[0][i-1], dp[0][i-2]) + stickers[1][i];
+            }
+
+            sb.append(Math.max(dp[0][n], dp[1][n]) + "\n");
+        }
+
+        System.out.println(sb.toString().trim());
+
+    }
+}
