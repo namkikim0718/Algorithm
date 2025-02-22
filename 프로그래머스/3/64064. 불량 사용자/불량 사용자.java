a55fb3d82163826fb12bcea44a0cb258a9f81015
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2025-02-22 15:27:14 +0900

    [level 3] Title: 불량 사용자, Time: 69.71 ms, Memory: 93.2 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
@@ -2,44 +2,54 @@ import java.util.*;
 
 class Solution {
     
-    static boolean[] isSelected;
-    static Set<String> set = new HashSet<>();
+    static Set<List<String>> result = new HashSet<>();
     
-    public static boolean check(String A, String B) {
-        if (A.length() != B.length()) {
-            return false;
-        } else {
-            for (int i = 0; i < A.length(); i++) {
-                if (A.charAt(i) != B.charAt(i) && B.charAt(i) != '*') {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-    
-    public static void dfs(String[] userId, String[] bannedId, int idx, String str) {
-        if (idx == bannedId.length) {
-            char[] charArr = str.toCharArray();
-            Arrays.sort(charArr);
-            set.add(new String(charArr));
+    static void dfs(String[] user_id, String[] banned_id, int depth, List<String> myList) {
+        
+        List<String> myNewList = new ArrayList<>(myList);
+        
+        if (depth == banned_id.length) {
+            Collections.sort(myNewList);
+            result.add(new ArrayList<>(myNewList));
             return;
         }
-        for (int i = 0; i < userId.length; i++) {
-            if (!isSelected[i] && check(userId[i], bannedId[idx])) {
-                isSelected[i] = true;
-                dfs(userId, bannedId, idx+1, str + i);
-                isSelected[i] = false;
+        
+        String nowBannedId = banned_id[depth];
+        for (int idx = 0; idx < user_id.length; idx++) {
+            // 이미 제제 아이디랑 일치하는 것으로 된 것은 넘어가도록
+            if (myList.contains(user_id[idx])) continue;
+            
+            String nowUserId = user_id[idx];
+            
+            // 현재 사용자가 가능한지 아닌지 나타내는 플래그
+            boolean isAvailable = true;
+            
+            // 문자열 길이가 다르면 애초에 같을 수 없음
+            if (nowUserId.length() != nowBannedId.length()) continue;
+            
+            // 문자 하나씩 비교
+            for (int i = 0; i < nowUserId.length(); i++) {
+                // 제제 아이디의 현재 문자가 *이면 넘어가도록
+                if (nowBannedId.charAt(i) == '*') continue;
+                if (nowUserId.charAt(i) != nowBannedId.charAt(i)) {
+                    isAvailable = false;
+                    break;
+                }
+            }
+            
+            // 현재 사용자 아이디가 가능하면 재귀로 탐색
+            if (isAvailable) {
+                myNewList.add(user_id[idx]);
+                dfs(user_id, banned_id, depth+1, myNewList);
+                myNewList.remove(myNewList.size() - 1);
             }
         }
     }
     
-    public int solution(String[] userId, String[] bannedId) {
-        isSelected = new boolean[userId.length];
-        String str = "";
+    public int solution(String[] user_id, String[] banned_id) {
+
+        dfs(user_id, banned_id, 0, new ArrayList<>());
         
-        dfs(userId, bannedId, 0, str);
-        
-        return set.size();
+        return result.size();
     }
 }
