d66ba7653a6ef9c0566bc4b1426e7545e84f3ee4
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2025-03-05 21:33:22 +0900

    [level 2] Title: H-Index, Time: 12.45 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -2,17 +2,26 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] citations) {
-        Arrays.sort(citations);
-
-        int hIndex = Integer.MIN_VALUE;
+        int n = citations.length;
         
-        for (int i = 0; i < citations.length; i++) {
-            int now = citations[i];
+        Arrays.sort(citations);
+        
+        int h = citations[n - 1];
+        
+        // h 이상 인용된 논문이 h개 이상이어야 한다.
+        // 나머지 논문이 모두 h번 이하 인용되어야 한다.
+        for (int i = h; i >= 0; i--) {
+            int upper = 0;
+            for (int p = n - 1; p >= 0; p--) {
+                if (citations[p] < i) break;
+                upper++;
+            }
             
-            int tmp = Math.min(citations.length - i, citations[i]);
-            hIndex = Math.max(hIndex, tmp);
+            if (upper >= i) {
+                return i;
+            }
         }
         
-        return hIndex;
+        return 0;
     }
 }
