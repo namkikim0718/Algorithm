dc05712558f79328c73a3533c2ea2789ea1de765
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2025-03-09 16:14:43 +0900

    [level 3] Title: 순위, Time: 12.33 ms, Memory: 92.5 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -4,40 +4,42 @@ class Solution {
     public int solution(int n, int[][] results) {
         int answer = 0;
         
+        // 뭔 알고리즘이더라. . .
+        // 경유지를 기준으로 돌려서 그래프를 채워넣자.
+        
         int[][] graph = new int[n+1][n+1];
         
+        // 1이면 승리, -1이면 패배.
         for (int[] result : results) {
             int winner = result[0];
             int loser = result[1];
-            
             graph[winner][loser] = 1;
             graph[loser][winner] = -1;
         }
         
-        // 경유하는 선수를 중심으로 해보자.
-        // A, B, C가 있을때
-        // A가 B를 이기고 B가 C를 이기면 A는 C를 이긴다
-        // A가 B에게 지고 B가 C에게 지면 A는 C에게 진다
-        for (int B = 1; B <= n; B++) {
-            for (int A = 1; A <= n; A++) {
-                for (int C = 1; C <= n; C++) {
-                    if (graph[A][B] == 1 && graph[B][C] == 1) {
-                        graph[A][C] = 1;
-                        graph[C][A] = -1;
-                    } else if (graph[A][B] == -1 && graph[B][C] == -1) {
-                        graph[A][C] = -1;
-                        graph[C][A] = 1;
+        // k선수가 i 선수를 이기면 i선수에게 진 선수는 모두 k선수에게 진다.
+        // k선수가 i 선수에게 지면 i선수에게 이긴 선수는 모두 k선수를 이긴다.
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (graph[k][i] == 1 && graph[i][j] == 1) {
+                        graph[k][j] = 1;
+                        graph[j][k] = -1;
+                    } else if (graph[k][i] == -1 && graph[i][j] == -1) {
+                        graph[k][j] = -1;
+                        graph[j][k] = 1;
                     }
                 }
             }
         }
-
-        for (int i = 1; i <= n; i++) {
-            int resultCnt = 0;
-            for (int j = 1; j <= n; j++) {
-                if (graph[i][j] != 0) resultCnt++;
+        
+        for (int[] row : graph) {
+            int cnt = 0;
+            for (int i = 1; i <= n; i++) {
+                if (row[i] == 0) cnt++;
             }
-            if (resultCnt == n - 1) answer++;
+            // 자기 자신말고 모두 승패 결과가 정해진 사람은 순위를 매길 수 있음.
+            if (cnt == 1) answer++;
         }
         return answer;
     }
