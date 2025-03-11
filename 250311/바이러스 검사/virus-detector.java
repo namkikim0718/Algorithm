bacc16648164f595e315c411ec1f32e20f67a176
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2025-03-11 15:01:20 +0900

    Title: 바이러스 검사, Time: 495ms, Memory: 70MB, Status: Wrong Answer - CodeTree

---
+++ 250311/바이러스 검사/virus-detector.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] custs = new int[n];
+        for (int i = 0; i < n; i++) {
+            custs[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+
+        int leader = Integer.parseInt(st.nextToken());
+        int member = Integer.parseInt(st.nextToken());
+
+        int memberCnt = 0;
+        for (int cust : custs) {
+            cust -= leader;
+            if (cust <= 0) continue;
+            memberCnt += cust / member;
+            if (cust % member != 0) memberCnt++;
+        }
+
+        System.out.println(memberCnt + n);
+
+    }
+}
