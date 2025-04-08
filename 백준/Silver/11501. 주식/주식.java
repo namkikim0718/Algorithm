0ebdbfc0ebfaff4edd4b3a9c6b0c3de822f681fa
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2025-04-08 14:18:29 +0900

    [Silver II] Title: 주식, Time: 1016 ms, Memory: 315348 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.java
+++ 백준/Silver/11501. 주식/주식.java
@@ -2,39 +2,39 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
     
     public static void main(String args[]) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int T = Integer.parseInt(br.readLine());
 
+        int T = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
+
         for (int t = 0; t < T; t++) {
             int N = Integer.parseInt(br.readLine());
 
             StringTokenizer st = new StringTokenizer(br.readLine());
-
             int[] days = new int[N];
             for (int i = 0; i < N; i++) {
                 days[i] = Integer.parseInt(st.nextToken());
             }
 
-            int maxPrice = 0;
+            int maxDay = 0;
             long profit = 0;
-            // 역방향 그리디
-            for (int i = N - 1; i >= 0; i--) {
-                if (maxPrice < days[i]) {
-                    maxPrice = days[i];
+            for (int i = N - 1; i >= 0 ; i--) {
+                
+                if (days[i] > maxDay) {
+                    maxDay = days[i];
                 } else {
-                    profit += maxPrice - days[i];
+                    profit += maxDay - days[i];
                 }
             }
 
-            sb.append(profit + "\n");
+            sb.append(profit).append("\n");
+
         }
 
         System.out.println(sb.toString().trim());
+
     }
 }
